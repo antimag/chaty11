30fa9c0f2d1511e2ead0a03d09c07298e6b4f8ab
rails <antima@grepruby.com>
2014-10-29 16:01:04 +0530

    added missing file private_pub

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  
-  has_many :conversations, :foreign_key => :sender_id         
+  has_many :conversations, :foreign_key => :sender_id   
+  def name
+    "#{self.firstname.to_s}  #{self.lastname.to_s}"
+  end      
 end
